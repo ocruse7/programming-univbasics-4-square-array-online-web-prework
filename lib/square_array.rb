fe0104811587d8018fbd99f24e560c3c30b243b1
Owen Cruse <ocruse7@gmail.com>
2019-10-24 16:52:09 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,5 +2,7 @@ def square_array(array)
   counter = 0
   
   while counter < array.length
-    array[counter] = array[counter]**
+    array[counter] = array[counter]**2
+    counter += 1
+  end
 end
